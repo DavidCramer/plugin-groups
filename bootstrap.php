3ba5cbae2c670d37a1e29d662f9cc4e501857556
David Cramer <david@digilab.co.za>
2021-08-20 10:19:16 +0200

    remove legacy

--- bootstrap.php
+++ bootstrap.php
@@ -12,11 +12,6 @@
  * Activate the plugin core.
  */
 function activate_plugin_groups() {
-
-	if( empty( get_option( '_plugin_groups_beta'))){
-		include PLGGRP_PATH . 'legacy/plugincore.php';
-		return;
-	}
 	// Include the core class.
 	include_once PLGGRP_PATH . 'classes/class-plugin-groups.php';
 	Plugin_Groups::get_instance();
